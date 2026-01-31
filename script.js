fb246f109117acb23c935f8eaaa97b60f472258a
Hyperxz08 <hyperha6@gmail.com>
2026-01-31 14:30:17 +0700

    update animation & effects

--- script.js
+++ script.js
@@ -9,28 +9,11 @@ document.getElementById("search").addEventListener("keyup", function () {
     });
 });
 
-// เพลง
-const music = document.getElementById("bgMusic");
-const btn = document.getElementById("musicBtn");
+// ===== เพลง =====
+const music = document.getElementById("bgm");
+const btn = document.getElementById("playMusic");
 
-btn.onclick = () => {
-    if (music.paused) {
-        music.play();
-        btn.textContent = "⏸ ปิดเพลง";
-    } else {
-        music.pause();
-        btn.textContent = "▶️ เปิดเพลง";
-    }
-};
-function toggleSection(id) {
-  const table = document.getElementById(id);
-  const title = event.target;
-
-  if (table.style.display === "none") {
-    table.style.display = "table";
-    title.innerHTML = title.innerHTML.replace("▶", "▼");
-  } else {
-    table.style.display = "none";
-    title.innerHTML = title.innerHTML.replace("▼", "▶");
-  }
-}
+btn.addEventListener("click", () => {
+  music.play();
+  btn.style.display = "none"; // ซ่อนปุ่มหลังคลิก
+});
